a8248254df7ca674defd3f2bd35e62d770bf82fe
fergbrain <fergbrain@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-21 22:02:59 +0000

    moved namespace from afdn to fergcorp Updated version number and copyright date
    git-svn-id: https://plugins.svn.wordpress.org/countdown-timer/trunk@220444 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Countdown Timer Uninstall Module
-Version 2.2.9.2 (kept in step with fergcorp_countdownTimer.php)
-Copyright (c) 2008 Andrew Ferguson
+Version 2.4 (kept in step with fergcorp_countdownTimer.php)
+Copyright (c) 2010 Andrew Ferguson
 ---------------------------------------------------------------------------------
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -24,8 +24,8 @@ if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
     exit();
 
 //Remove options from wp_options table
-delete_option('afdn_countdowntracker');
-delete_option('afdn_countdownOptions');
+delete_option('fergcorp_countdowntracker');
+delete_option('fergcorp_countdownOptions');
 delete_option('fergcorp_countdownTimer_version');
 delete_option('widget_fergcorp_countdown');
 
