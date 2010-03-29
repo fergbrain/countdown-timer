52e7ed8f0541029f3b0de7d9c84e6e63739b5b85
fergbrain <fergbrain@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-29 05:48:49 +0000

    Fixed ticket #940
    git-svn-id: https://plugins.svn.wordpress.org/countdown-timer/trunk@222856 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -24,8 +24,23 @@ if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
     exit();
 
 //Remove options from wp_options table
-delete_option('fergcorp_countdowntracker');
-delete_option('fergcorp_countdownOptions');
+delete_option('fergcorp_countdownTimer_deleteOneTimeEvents');
+delete_option('fergcorp_countdownTimer_checkUpdate');
+delete_option('fergcorp_countdownTimer_timeOffset');
+delete_option('fergcorp_countdownTimer_showYear');
+delete_option('fergcorp_countdownTimer_showMonth');
+delete_option('fergcorp_countdownTimer_showWeek');
+delete_option('fergcorp_countdownTimer_showDay');
+delete_option('fergcorp_countdownTimer_showHour');
+delete_option('fergcorp_countdownTimer_showMinute');
+delete_option('fergcorp_countdownTimer_showSecond');
+delete_option('fergcorp_countdownTimer_stripZero');
+delete_option('fergcorp_countdownTimer_enableJS');
+delete_option('fergcorp_countdownTimer_timeSinceTime');
+delete_option('fergcorp_countdownTimer_titleSuffix');
+delete_option('fergcorp_countdownTimer_serialDataFilename');
+delete_option('fergcorp_countdownTimer_enableShortcodeExcerpt');
+delete_option('fergcorp_countdownTimer_oneTimeEvent');
 delete_option('fergcorp_countdownTimer_version');
 delete_option('widget_fergcorp_countdown');
 
