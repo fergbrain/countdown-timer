afd92ff753dd5e9f81b9d89a79e5133e49bdb348
fergbrain <fergbrain@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-23 06:04:41 +0000

    Getting started on version 2.5. Testing using Aptana and Subclipse
    git-svn-id: https://plugins.svn.wordpress.org/countdown-timer/trunk@349608 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fergcorp_countdownTimer.php
+++ fergcorp_countdownTimer.php
@@ -3,12 +3,12 @@
 Plugin Name: Countdown Timer
 Plugin URI: http://www.andrewferguson.net/wordpress-plugins/countdown-timer/
 Plugin Description: Add template tags and widget to count down or up to the years, months, weeks, days, hours, minutes, and/or seconds to a particular event.
-Version: 2.4.2
+Version: 2.5 Alpha
 Author: Andrew Ferguson
 Author URI: http://www.andrewferguson.net
 
 Countdown Timer - Add template tags and widget to count down the years, months, weeks, days, hours, and minutes to a particular event
-Copyright (c) 2005-2010 Andrew Ferguson
+Copyright (c) 2005-2011 Andrew Ferguson
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
