4ffb30e6ea3fcb07e98210f810f297f54b9273aa
Andrew Ferguson <andrew@fergcorp.com>
2012-10-24 09:33:58 +0200

    76.13% code coverage

--- tests/CountDownTimerTest.php
+++ tests/CountDownTimerTest.php
@@ -447,53 +447,209 @@ class CountdownTimerTest extends WP_UnitTestCase {
 	
 	
 	public function test_formatEvent(){
-		//@TODO
+
+
+
+
+		$this->assertRegExp("/^<abbr title = \"(.*?)\" id = 'x[0-9a-z]{32}' class = 'fergcorp_countdownTimer_event_time'>in <span class=\"fergcorp_countdownTimer_day fergcorp_countdownTimer_timeUnit\">1 day,<\/span> <span class=\"fergcorp_countdownTimer_hour fergcorp_countdownTimer_timeUnit\">0 hours,<\/span> <span class=\"fergcorp_countdownTimer_minute fergcorp_countdownTimer_timeUnit\">0 minutes<\/span><\/abbr>$/is", $this->plugin->formatEvent( new Fergcorp_Countdown_Timer_Event( strtotime( "+1day" ) ) , TRUE ));
+		$this->assertRegExp("/^<abbr title = \"(.*?)\" id = 'x[0-9a-z]{32}' class = 'fergcorp_countdownTimer_event_time'><span class=\"fergcorp_countdownTimer_day fergcorp_countdownTimer_timeUnit\">1 day,<\/span> <span class=\"fergcorp_countdownTimer_hour fergcorp_countdownTimer_timeUnit\">0 hours,<\/span> <span class=\"fergcorp_countdownTimer_minute fergcorp_countdownTimer_timeUnit\">0 minutes<\/span> ago<\/abbr>$/is", $this->plugin->formatEvent( new Fergcorp_Countdown_Timer_Event( strtotime( "-1day" ) ) , TRUE ));
 		
-		//Setup massive database of events!!!!
-		$event_object_array = array();
-		//Years
-		
-		$time = new DateTime();
-		$time->setDate(2023, 1, 31);
-		$time->setTime(11, 37, 53);
-		
-		for($i = 1; $i < 10; $i++){
-			$time->sub(new DateInterval("P1Y"));
-			array_push($event_object_array, new Fergcorp_Countdown_Timer_Event($time->getTimestamp(), "Date is set to " . $time->format('Y-m-d H:i:s'), "http://example.com", 0));
-		}
-		
-		for($i = 1; $i < 12; $i++){
-			$time->sub(new DateInterval("P1M"));
-			array_push($event_object_array, new Fergcorp_Countdown_Timer_Event($time->getTimestamp(), "Date is set to " . $time->format('Y-m-d H:i:s'), "http://example.com", 0));
-		}
-		
-		for($i = 1; $i < date("t", $time->getTimestamp()); $i++){
-			$time->sub(new DateInterval("P1D"));
-			array_push($event_object_array, new Fergcorp_Countdown_Timer_Event($time->getTimestamp(), "Date is set to " . $time->format('Y-m-d H:i:s'), "http://example.com", 0));
-		}
-		
-		for($i = 0; $i < 23; $i++){
-			$time->sub(new DateInterval("PT1H"));
-			array_push($event_object_array, new Fergcorp_Countdown_Timer_Event($time->getTimestamp(), "Date is set to " . $time->format('Y-m-d H:i:s'), "http://example.com", 0));
-		}
-		
-		for($i = 0; $i < 59; $i++){
-			$time->sub(new DateInterval("PT1M"));
-			array_push($event_object_array, new Fergcorp_Countdown_Timer_Event($time->getTimestamp(), "Date is set to " . $time->format('Y-m-d H:i:s'), "http://example.com", 0));
-		}
-					
-		for($i = 0; $i < 59; $i++){
-			$time->sub(new DateInterval("PT1S"));
-			array_push($event_object_array, new Fergcorp_Countdown_Timer_Event($time->getTimestamp(), "Date is set to " . $time->format('Y-m-d H:i:s'), "http://example.com", 0));
-		}
-		
-				update_option("fergcorp_countdownTimer_oneTimeEvent", $event_object_array);
-		$this->plugin->loadSettings();
-		
+	$this->assertRegExp("/^<li class = 'fergcorp_countdownTimer_event_li'><abbr title = \"(.*?)\" id = 'x[0-9a-z]{32}' class = 'fergcorp_countdownTimer_event_time'>in <span class=\"fergcorp_countdownTimer_day fergcorp_countdownTimer_timeUnit\">1 day,<\/span> <span class=\"fergcorp_countdownTimer_hour fergcorp_countdownTimer_timeUnit\">0 hours,<\/span> <span class=\"fergcorp_countdownTimer_minute fergcorp_countdownTimer_timeUnit\">0 minutes<\/span><\/abbr><\/li>\r\n$/is", $this->plugin->formatEvent( new Fergcorp_Countdown_Timer_Event( strtotime( "+1 day" ) ) , FALSE ));
+	
+	$this->assertRegExp("/^<li class = 'fergcorp_countdownTimer_event_li'><abbr title = \"(.*?)\" id = 'x[0-9a-z]{32}' class = 'fergcorp_countdownTimer_event_time'>in <span class=\"fergcorp_countdownTimer_day fergcorp_countdownTimer_timeUnit\">1 day,<\/span> <span class=\"fergcorp_countdownTimer_hour fergcorp_countdownTimer_timeUnit\">0 hours,<\/span> <span class=\"fergcorp_countdownTimer_minute fergcorp_countdownTimer_timeUnit\">0 minutes<\/span><\/abbr><\/li>\r\n$/is", $this->plugin->formatEvent( new Fergcorp_Countdown_Timer_Event( strtotime( "yesterday" ) ) , FALSE ));
+
+	
 	}
 	
 	public function test_fuzzyDate(){
-		//@TODO
+		$now = time();
+		
+				$s = array(		"<span class=\"fergcorp_countdownTimer_year fergcorp_countdownTimer_timeUnit\">",
+								"year",
+								"<span class=\"fergcorp_countdownTimer_month fergcorp_countdownTimer_timeUnit\">",
+								"month",
+								"<span class=\"fergcorp_countdownTimer_week fergcorp_countdownTimer_timeUnit\">",
+								"week",
+								"<span class=\"fergcorp_countdownTimer_day fergcorp_countdownTimer_timeUnit\">",
+								"day",
+								"<span class=\"fergcorp_countdownTimer_hour fergcorp_countdownTimer_timeUnit\">",
+								"hour",
+								"<span class=\"fergcorp_countdownTimer_minute fergcorp_countdownTimer_timeUnit\">",
+								"minute",
+								"<span class=\"fergcorp_countdownTimer_second fergcorp_countdownTimer_timeUnit\">",
+								"second",
+								"<\/span>",						
+		);
+
+		//Setup massive database of events!!!!
+		$unitList = array(	"Year",
+							"Month",
+							//"Week",
+							"Day",
+							"Hour",
+							"Minute",
+							"Second",
+		);
+		
+		$unitList_reverse = array_reverse($unitList);
+		
+		foreach($unitList as $unit){
+				update_option("fergcorp_countdownTimer_show{$unit}", 0);
+			}
+		
+
+			function check_this($time, $unitList, $s, $now){
+				
+				$nowObj = new DateTime;
+				date_timestamp_set($nowObj, $now);
+				
+				$diff = date_diff($time, $nowObj);
+				
+				$secondsDiff = $time->getTimestamp() - $now;
+	
+				$Year =		$s[0].intval($secondsDiff/31536000)." ".$s[1]."s?,".$s[14]." ";
+				$Month =	$s[2].intval($secondsDiff/2628000)." ".$s[3]."s?,".$s[14]." ";
+				$Week =		$s[4].intval($secondsDiff/2628000)." ".$s[5]."s?,".$s[14]." ";
+				$Day = 		$s[6].intval($secondsDiff/86400)." ".$s[7]."s?,".$s[14]." ";
+				$Hour =		$s[8].intval($secondsDiff/3600)." ".$s[9]."s?,".$s[14]." ";
+				$Minute =	$s[10].intval($secondsDiff/60)." ".	$s[11]."s?,".$s[14]." ";
+				$Second = 	$s[12].$secondsDiff." ".		$s[13]."(s)?,".$s[14]." ";
+				
+				$secondFudge = "(".($diff->s-1)."|".$diff->s."|".($diff->s+1).")"; //Test takes too long!
+				
+				if(get_option("fergcorp_countdownTimer_showYear")){
+					$Year =		$s[0].$diff->y." ".$s[1]."(s)?,".$s[14]." ";
+					$Month =	$s[2].$diff->m." ".$s[3]."s?,".$s[14]." ";
+					$Day = 		$s[6].$diff->d." ".$s[7]."s?,".$s[14]." ";
+					$Hour =		$s[8].$diff->h." ".$s[9]."s?,".$s[14]." ";
+					$Minute =	$s[10].$diff->i." ".$s[11]."s?,".$s[14]." ";
+					$Second = 	$s[12].$secondFudge." ".$s[13]."s?,".$s[14]." ";
+				}			
+				elseif(get_option("fergcorp_countdownTimer_showMonth")){
+					$secondsDiff = $secondsDiff - ($diff->m*2628000);
+					$Day = 		$s[6].$diff->d." ".$s[7]."s?,".$s[14]." ";
+					$Hour =		$s[8].$diff->h." ".$s[9]."s?,".$s[14]." ";
+					$Minute =	$s[10].$diff->i." ".$s[11]."s?,".$s[14]." ";
+					$Second = 	$s[12].$secondFudge." ".$s[13]."s?,".$s[14]." ";
+				}
+				elseif(get_option("fergcorp_countdownTimer_showWeek")){
+					$Week = 	$s[4].intval($diff->days/7)." ".$s[5]."s?,".$s[14]." ";
+					$Day = 		$s[6].($diff->d%7)." ".$s[7]."s?,".$s[14]." ";
+					$Hour =		$s[8].$diff->h." ".$s[9]."s?,".$s[14]." ";
+					$Minute =	$s[10].$diff->i." ".$s[11]."s?,".$s[14]." ";
+					$Second = 	$s[12].$secondFudge." ".$s[13]."s?,".$s[14]." ";
+				}	
+				elseif(get_option("fergcorp_countdownTimer_showDay")){
+					$secondsDiff = $secondsDiff - ($diff->d*86400);
+					$Hour =		$s[8].$diff->h." ".$s[9]."s?,".$s[14]." ";
+					$Minute =	$s[10].$diff->i." ".$s[11]."s?,".$s[14]." ";
+					$Second = 	$s[12].$secondFudge." ".$s[13]."s?,".$s[14]." ";
+				}
+				elseif(get_option("fergcorp_countdownTimer_showHour")){
+					$secondsDiff = $secondsDiff - ($diff->h*3600);
+					$Minute =	$s[10].$diff->i." ".	$s[11]."s?,".$s[14]." ";
+					$Second = 	$s[12].$secondFudge." ".		$s[13]."s?,".$s[14]." ";
+				}
+				elseif(get_option("fergcorp_countdownTimer_showMinute")){
+					$secondsDiff = $secondsDiff - ($diff->s*60);
+					$Second = 	$s[12].$secondFudge." ".		$s[13]."s?,".$s[14]." ";
+				}
+				foreach($unitList as $unit){
+					if(get_option("fergcorp_countdownTimer_show{$unit}")){
+						$unitValue[$unit] .= ${$unit};
+					}
+				}
+				
+				$testOfUnits = "";
+				foreach($unitList as $unit){
+					if(get_option("fergcorp_countdownTimer_show{$unit}")){
+						$testOfUnits .= ${$unit};
+					}
+				}
+				//print $testOfUnits."\n\n";
+				return $testOfUnits;
+			}
+		
+	
+		for($u=count($unitList)-1; $u >= 0; $u--){
+			update_option("fergcorp_countdownTimer_show{$unitList[$u]}", 1);
+			$this->plugin->loadSettings();
+			$time = new DateTime("+11 years, 9 months, 7 days, 5 hours, 3 minutes, 1 second");
+			
+			//Years
+			for($i = 10; $i > 2; $i--){
+				$time->sub(new DateInterval("P1Y"));
+				$this->assertRegExp("/^{$header}".preg_replace("/,<\\\\\/span> $/", "<\/span>", check_this($time, $unitList, $s, $now))."/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));	
+			}
+
+			for($i = 1; $i < 12; $i++){
+				$time->sub(new DateInterval("P1M"));
+				$this->assertRegExp("/^{$header}".preg_replace("/,<\\\\\/span> $/", "<\/span>", check_this($time, $unitList, $s, $now))."/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));	
+			}
+			
+			for($i = 1; $i < date("t", $time->getTimestamp()); $i++){
+				$time->sub(new DateInterval("P1D"));
+				$this->assertRegExp("/^{$header}".preg_replace("/,<\\\\\/span> $/", "<\/span>", check_this($time, $unitList, $s, $now))."/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));	
+			}
+		
+		
+			for($i = 0; $i < 25; $i++){
+				$time->sub(new DateInterval("PT1H"));
+				$this->assertRegExp("/^{$header}".preg_replace("/,<\\\\\/span> $/", "<\/span>", check_this($time, $unitList, $s, $now))."/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));	
+			}
+			
+			for($i = 0; $i < 59; $i++){
+				$time->sub(new DateInterval("PT1M"));
+				$this->assertRegExp("/^{$header}".preg_replace("/,<\\\\\/span> $/", "<\/span>", check_this($time, $unitList, $s, $now))."/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));	
+			}
+			
+			for($i = 0; $i < 59; $i++){
+				$time->sub(new DateInterval("PT1S"));
+				$this->assertRegExp("/^{$header}".preg_replace("/,<\\\\\/span> $/", "<\/span>", check_this($time, $unitList, $s, $now))."/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));
+			}
+	
+			}
+			
+		//some special cases
+		$time = new DateTime("+11 years, 9 months, 7 days, 5 hours, 3 minutes, 1 second");
+		$now = time();
+		//No month, with year, and every thing else
+		update_option("fergcorp_countdownTimer_showMonth", 0);
+		$this->plugin->loadSettings();
+		print $this->plugin->fuzzyDate($time->getTimestamp(), $now);
+		$this->assertRegExp("/^<span class=\"fergcorp_countdownTimer_year fergcorp_countdownTimer_timeUnit\">11 year(s)?,<\/span> <span class=\"fergcorp_countdownTimer_day fergcorp_countdownTimer_timeUnit\">283 days?,<\/span> <span class=\"fergcorp_countdownTimer_hour fergcorp_countdownTimer_timeUnit\">5 hours?,<\/span> <span class=\"fergcorp_countdownTimer_minute fergcorp_countdownTimer_timeUnit\">3 minutes?,<\/span> <span class=\"fergcorp_countdownTimer_second fergcorp_countdownTimer_timeUnit\">(1|2|3) seconds?<\/span>/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));
+		
+		
+		//Weeks
+				$unitList = array(	"Year",
+							"Month",
+							"Week",
+							"Day",
+							"Hour",
+							"Minute",
+							"Second",
+		);
+		update_option("fergcorp_countdownTimer_showYear", 0);
+		update_option("fergcorp_countdownTimer_showMonth", 0);
+		update_option("fergcorp_countdownTimer_showWeek", 1);
+		$this->plugin->loadSettings();
+		print $this->plugin->fuzzyDate($time->getTimestamp(), $now);
+		$this->assertRegExp("/^{$header}".preg_replace("/,<\\\\\/span> $/", "<\/span>", check_this($time, $unitList, $s, $now))."/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));
+		
+		//No month, with year, and every thing else
+		update_option("fergcorp_countdownTimer_showYear", 0);
+		update_option("fergcorp_countdownTimer_showMonth", 0);
+		update_option("fergcorp_countdownTimer_showWeek", 0);
+		update_option("fergcorp_countdownTimer_showDay", 0);
+		update_option("fergcorp_countdownTimer_showHow", 0);
+		update_option("fergcorp_countdownTimer_showMinute", 0);
+		update_option("fergcorp_countdownTimer_showSecond", 0);
+
+		$this->plugin->loadSettings();
+		print $this->plugin->fuzzyDate($time->getTimestamp(), $now);
+		$this->assertRegExp("/^{$header}".preg_replace("/,<\\\\\/span> $/", "<\/span>", check_this($time, $unitList, $s, $now))."/is", $this->plugin->fuzzyDate($time->getTimestamp(), $now));
+		
+
 	}
 	
 	public function test_shortcode_showTimer(){
