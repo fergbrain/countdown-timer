50376418ef086bed66a45215d133f8092f47b754
Andrew Ferguson <andrewferguson@helios.local>
2013-01-13 02:51:35 +0100

    * Cleaned up some debug code * Rev to Beta

--- fergcorp_countdownTimer.php
+++ fergcorp_countdownTimer.php
@@ -3,7 +3,7 @@
 Plugin Name: Countdown Timer
 Plugin URI: http://www.andrewferguson.net/wordpress-plugins/countdown-timer/
 Description: Use shortcodes and a widget to count down or up to the years, months, weeks, days, hours, minutes, and/or seconds to a particular event.
-Version: 3.0.4 Alpha
+Version: 3.0.4 Beta
 Author: Andrew Ferguson
 Author URI: http://www.andrewferguson.net
 
@@ -1305,7 +1305,6 @@ class Fergcorp_Countdown_Timer_Event extends DateTime {
 	}
 
 	public function date ( $format ) {
-		//FB::info($this->getTimestamp(), "Timestamp");
 		return date($format, $this->getTimestamp());
 	}
 }
