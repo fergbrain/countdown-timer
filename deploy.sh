37b1468deb1e7a8b78ea2b030689b2835c39916e
Andrew Ferguson <andrew@fergcorp.com>
2017-06-13 14:32:20 +0100

    Updated deploy script to fix svn: E120105 error
    This seems to fix “svn: E120105: Error retrieving REPORT: The server
    sent an improper HTTP response”

--- deploy.sh
+++ deploy.sh
@@ -48,7 +48,7 @@ git push origin master --tags
 
 echo
 echo "Creating local copy of SVN repo ..."
-svn co $SVNURL $SVNPATH
+svn co $SVNURL/trunk $SVNPATH/trunk
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
